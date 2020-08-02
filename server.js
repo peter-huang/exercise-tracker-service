42a8366564264c2c0929284a243d64d6f8d3667d
Peter Huang <peterhonline@gmail.com>
2020-08-02 01:18:12 -0400

    Updated package.json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const crypto = require("crypto");
 const mongoose = require("mongoose");
 
 // Setup MongoDB
-process.env.MONGO_URI = "YourMongoDbAuthentication";
+process.env.MONGO_URI = "YourMongoDBAuthentication";
 var db = mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
