51ac5014d72c850c8c3cc13815060a741c1ad767
Peter Huang <peterhonline@gmail.com>
2020-09-23 10:42:17 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const crypto = require("crypto");
 const mongoose = require("mongoose");
 
 // Setup MongoDB
-process.env.MONGO_URI = "YourMongoDBAuthentication";
+process.env.MONGO_URI =
+  "mongodb+srv://fccmongotest:fccmongotest@peter0.xkbla.mongodb.net/<dbname>?retryWrites=true&w=majority";
 var db = mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
