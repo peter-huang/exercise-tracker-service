f74d272ed048ce1767b5c95a03c493162e7f60d4
Peter Huang <peterhonline@gmail.com>
2020-09-23 10:45:11 -0400

    Reverted server.js mongodb auth

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const crypto = require("crypto");
 const mongoose = require("mongoose");
 
 // Setup MongoDB
-process.env.MONGO_URI =
-  "mongodb+srv://fccmongotest:fccmongotest@peter0.xkbla.mongodb.net/<dbname>?retryWrites=true&w=majority";
+process.env.MONGO_URI = "YourMongoDBAuthentication";
 var db = mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
